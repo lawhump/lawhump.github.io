996fe40e41129b7da6780e94a3e0f5fc55e7e335
Lawrence Humphrey <lawrenceh1993@gmail.com>
2015-06-23 16:44:38 -0500

    damn it all

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
 $( document ).ready(function() {
-	// if((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
-	// 	$('.landing').css("background", "url(../img/path_iphone.jpeg) no-repeat center center fixed");
-	// }
-
 	window.onload = function () {
 		$( ".anchor i" ).addClass('animated fadeInDown').removeClass('invisible');
 	};
-
+	
    $(function() {
 		$('a[href*=#]:not([href=#])').click(function() {
 			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
