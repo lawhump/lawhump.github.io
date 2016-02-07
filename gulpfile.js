7e08828b0912e8b2617c805f751d956cef5b47f7
Lawrence Humphrey <lawrence@Lawrences-MacBook-Pro-2.local>
2016-02-07 15:56:59 -0600

    people don't optimize for screen real estate ughhhh

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,29 @@
-// Include gulp
-var gulp = require('gulp'); 
-
-// Include Our Plugins
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
-var uglify = require('gulp-uglify');
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
 var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var server = require('gulp-webserver');
 
 
-// Compile Our Sass
+// Concatenate and compile SCSS
 gulp.task('sass', function() {
-    gulp.src('css/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css'));
-});
-
-gulp.task('styles', function() {
-    gulp.src(['css/normalize.css', 'css/styles.css'])
-        .pipe(concat('all.css'))
-        .pipe(gulp.dest('dist/css'))
-        .pipe(minifyCSS())
-        .pipe(rename('all.min.css'))
-        .pipe(gulp.dest('dist/css'));
+  return gulp.src('dev/scss/main.scss')
+    .pipe($.sass()
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('dev/css'))
+    .pipe(cssnano())
+    .pipe(rename('all.min.css'))
+    .pipe(gulp.dest('dist/css'));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('js/*.js')
+    return gulp.src(['dev/js/**/*.js', '!dev/js/vendor/**'])
         .pipe(concat('all.js'))
         .pipe(gulp.dest('dist/js'))
         .pipe(rename('all.min.js'))
@@ -35,12 +31,10 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('dist/js'));
 });
 
-// Watch Files For Changes
-gulp.task('watch', function() {
-    gulp.watch('js/*.js', ['scripts']);
-    // gulp.watch('css/*.scss', ['sass', 'styles']);
-    gulp.watch('css/**/*.scss', ['sass', 'styles']);
+gulp.task('default', ['sass', 'scripts'], function() {
+  gulp.watch(['dev/scss/**/*.scss'], ['sass']);
+  gulp.watch('dev/js/*.js', ['scripts']);
 });
 
-// Default Task
-gulp.task('default', ['sass', 'scripts', 'styles', 'watch']);
+gulp.task('build', ['sass', 'scripts']);
+
